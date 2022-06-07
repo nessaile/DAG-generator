08511804e7c27c9b60ed6f4a4d6a053d543623eb
E. E <nessaile@gmail.com>
2022-06-07 11:57:19 +0200

    Update DAG_generator.jl

--- DAG_generator.jl
+++ DAG_generator.jl
@@ -1,5 +1,5 @@
 import Pkg
-using Combinatorics, LinearAlgebra, Base.Threads, Octavian
+using Combinatorics, LinearAlgebra, Base.Threads, Octavian, Chain
 
 function DAG_space_gen(n)
     @assert n > 1 "Generates all possible DAGs for n > 1"
@@ -36,7 +36,7 @@ function DAG_space_gen(n)
     @threads for t in 1:threads
         threadID = threads > 1 ? Threads.threadid() : 1
 
-        upper_triangle = reshape(repeat(Int8(1):Int8(n), n), (n, n)) |> x -> x .< x'
+        upper_triangle = @chain reshape(repeat(Int8(1):Int8(n), n), (n, n)) _ .< _'
 
         for i in chunks[threadID][1]:chunks[threadID][2]
             base_matrix[upper_triangle] = collect(choices[i])
